86bc18d177f737812c402cdacf97cda0f8e16597
Raphael Brand <info@raphaelbrand.de>
2020-02-20 19:03:02 +0100

    add random number sets

--- main.js
+++ main.js
@@ -3,12 +3,62 @@ import ReactDOM from 'react-dom';
 
 import { GameUI } from './src/components/GameUI';
 
+
+
 class MathGame extends Component {
+
+
+    constructor(props) {
+        super(props);
+        const sum_min = 2;
+        const sum_max = 27;
+        this.state = {
+            sum_range: [sum_min, sum_max],
+            sum_min: sum_min,
+            sum_max: sum_max,
+        };
+
+        this.sets = new Array();
+    }
+
+
+    initNumbers(amount) {
+        const arr = [];
+        let sum = 0;
+        let index = 0;
+        let val;
+        while (sum < this.state.sum_max && index < amount) {
+            for (let i = 0; arr.length < amount; i++ , index++) {
+                val = Math.floor((8 * Math.random()) + 1);
+                sum += val;
+
+                if (arr.toString().indexOf(val) > -1) {
+                    (sum = sum - val);
+                    continue;
+                }
+
+                arr.push(val);
+            }
+        }
+
+        return arr;
+    }
+
+    initNumberSets(amount) {
+        this.sets = [];
+        for (let i = 0; i < amount; i++) {
+            this.sets.push(this.initNumbers(amount));
+        }
+
+        return this.sets;
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Hello JSX</h1>
-                <GameUI />
+                <GameUI matrix={this.initNumberSets(3)} />
             </div>
         );
     }
--- src/components/GameUI.jsx
+++ src/components/GameUI.jsx
@@ -3,13 +3,9 @@ import { Playfield } from './Playfield';
 
 export class GameUI extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
-            <Playfield matrix={[[1, 2, 3], [1, 2, 3], [1, 2, 3]]} >
+            <Playfield matrix={this.props.matrix}>
                 <div className="test">Math Game</div>
             </Playfield>);
     }
